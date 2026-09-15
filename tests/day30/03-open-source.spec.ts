ef217a09cf2fb729ed9852c1791bf6c9ce242939
Sinem <sinemayk@gmail.com>
2026-09-15 14:49:13 +0200

    Added all pages to custom fixtures

--- tests/day30/03-open-source.spec.ts
+++ tests/day30/03-open-source.spec.ts
@@ -1,13 +1,10 @@
-import { test, expect } from '@playwright/test';
-import { OpenSourcePage } from '../../pages/OpenSourcePage';
+import { test, expect } from "@playwright/test";
+import { OpenSourcePage } from "../../pages/OpenSourcePage";
 
-test('login test', async ({ page }) => {
+test("login test", async ({ page }) => {
+  const loginPage = new OpenSourcePage(page);
 
-    const loginPage = new OpenSourcePage(page);
-
-    await loginPage.goto();
-    await loginPage.login();
-    await expect(loginPage.dashboard).toBeVisible();
-
-    
+  await loginPage.goto();
+  await loginPage.login();
+  await expect(loginPage.dashboard).toBeVisible();
 });
--- tests/day32/03-custom-fixture.spec.ts
+++ tests/day32/03-custom-fixture.spec.ts
@@ -1,8 +1,26 @@
 import { test, expect } from "../../fixtures/pageFixtures";
+import { FakerHelper } from "../../utils/testDataHelper";
 
 test("POM Usage", async ({ sauceLoginPage, page }) => {
   await sauceLoginPage.gotoLoginPage();
   await sauceLoginPage.login();
 
   await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+});
+
+test("Register test", async ({ registerPage }) => {
+  await registerPage.goto();
+  const user = FakerHelper.createUser();
+
+  await registerPage.fillForm(user);
+  await registerPage.acceptPrivacyPolicy();
+  await registerPage.clickContinueButton();
+
+  await registerPage.assertRegistrationSuccess();
+});
+
+test("login test", async ({ openSourcePage }) => {
+  await openSourcePage.goto();
+  await openSourcePage.login();
+  await expect(openSourcePage.dashboard).toBeVisible();
 });
