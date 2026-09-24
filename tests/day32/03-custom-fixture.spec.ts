e2b9321d2ba2f8744ce21a9cab4bca3f45614c4c
Sinem <sinemayk@gmail.com>
2026-09-24 09:42:49 +0200

    timeout added for flaky tests

--- tests/day32/03-custom-fixture.spec.ts
+++ tests/day32/03-custom-fixture.spec.ts
@@ -22,6 +22,6 @@ test("Register test", async ({ registerPage }) => {
 test("login test", async ({ openSourcePage }) => {
   await openSourcePage.goto();
   await openSourcePage.login();
-  await expect(openSourcePage.dashboard).toBeVisible({ timeout: 10000 });
+  await expect(openSourcePage.dashboard).toBeVisible({ timeout: 15000 });
 
 });
